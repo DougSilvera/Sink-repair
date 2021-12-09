4e321902ce37df236cd890af471ed3ee85ca3db9
DougSilvera <shortxjdoug@gmail.com>
2021-12-09 16:58:48 -0500

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+import { fetchRequests } from "./dataAccess.js"
+import { SinkRepair } from "./SinkRepair.js"
+import { deleteRequest } from "./dataAccess.js"
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
+
+mainContainer.addEventListener(
+    "stateChanged",
+    customEvent => {
+        render()
+    }
+)
+
+const render = () => {
+    fetchRequests().then(
+        () => {
+            mainContainer.innerHTML = SinkRepair()
+        }
+    )
+}
+
+render()
