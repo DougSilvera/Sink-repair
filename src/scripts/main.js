331d7b711bc57a83075cd4cea6575327e94f0fc1
DougSilvera <shortxjdoug@gmail.com>
2021-12-13 17:03:50 -0500

    more functionality and final touches

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,24 @@
-import { fetchRequests } from "./dataAccess.js"
-import { SinkRepair } from "./SinkRepair.js"
-import { deleteRequest } from "./dataAccess.js"
+import { fetchRequests } from "./dataAccess.js";
+import { SinkRepair } from "./SinkRepair.js";
+import { deleteRequest } from "./dataAccess.js";
 
-const mainContainer = document.querySelector("#container")
+const mainContainer = document.querySelector("#container");
 
-mainContainer.addEventListener("click", click => {
-    if (click.target.id.startsWith("request--")) {
-        const [,requestId] = click.target.id.split("--")
-        deleteRequest(parseInt(requestId))
-    }
-})
+mainContainer.addEventListener("click", (click) => {
+  if (click.target.id.startsWith("request--")) {
+    const [, requestId] = click.target.id.split("--");
+    deleteRequest(parseInt(requestId));
+  }
+});
 
-mainContainer.addEventListener(
-    "stateChanged",
-    customEvent => {
-        render()
-    }
-)
+mainContainer.addEventListener("stateChanged", (customEvent) => {
+  render();
+});
 
 const render = () => {
-    fetchRequests().then(
-        () => {
-            mainContainer.innerHTML = SinkRepair()
-        }
-    )
-}
+  fetchRequests().then(() => {
+    mainContainer.innerHTML = SinkRepair();
+  });
+};
 
-render()
+render();
